b36b917a91adce5adb6c18dcea78597953f3ec31
Marcel <Marcel@130.229.180.165>
2012-11-19 10:37:35 +0100

    Database created

---
+++ id2212-hw3bis/src/id2212/hw3/database/DataBase.java
@@ -0,0 +1,49 @@
+package id2212.hw3.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marcel
+ */
+public class DataBase {
+    
+    private Connection conn;
+    private Statement statement;
+    private String datasource = "catalogdatabase";
+    private String user = "root";
+    private String passwd = "root";
+    private boolean initialized=false;
+    
+    
+    public DataBase() {
+    }
+    
+    public void connectDatabase() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + datasource, user, passwd);
+	statement = conn.createStatement();
+        initialized=true;
+	System.out.println("Connected to database..." + conn.toString());
+    }
+    
+    public void close() throws Exception {
+	if (initialized) {
+            conn.close();
+            initialized=false;
+        }
+        System.out.println();
+        System.out.println("Connection closed...");
+    }
+    
+    public Register getRegisterInstance() {
+        return new Register(conn, statement);
+    }
+    
+}
---
+++ id2212-hw3bis/src/id2212/hw3/database/Files.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id2212.hw3.database;
+
+/**
+ *
+ * @author Marcel
+ */
+public class Files {
+    
+}
