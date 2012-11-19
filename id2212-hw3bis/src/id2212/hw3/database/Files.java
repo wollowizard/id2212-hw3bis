3721d1c7bfab8b9a5128d9ea57f7d6beffbfec09
Marcel <Marcel@130.229.180.165>
2012-11-19 11:11:08 +0100

    Database done

--- id2212-hw3bis/src/id2212/hw3/database/Files.java
+++ id2212-hw3bis/src/id2212/hw3/database/Files.java
@@ -21,11 +21,17 @@ public class Files {
     private Statement statement;
     private PreparedStatement insert;
     private PreparedStatement delete;
-    private PreparedStatement update;
-    private PreparedStatement select;
-    private PreparedStatement selectConected;
+    private PreparedStatement updateSize;
+    private PreparedStatement updatePrivacy;
+    private PreparedStatement updatePermission;
+    private PreparedStatement selectByName;
+    private PreparedStatement selectBySize;
+    private PreparedStatement selectByOwner;
+    private PreparedStatement selectByPrivacy;
+    private PreparedStatement selectByNameAndOwner;
     
     public static final String DB_NAME = "Files";
+    
     
     public Files(Connection conn, Statement s) {
         this.conn=conn;
@@ -45,51 +51,83 @@ public class Files {
     public void createRegister() throws SQLException {
         statement.executeUpdate(
                 "CREATE TABLE "+DB_NAME+" (name VARCHAR(255) NOT NULL PRIMARY KEY, "
-                + "size int, owner VARCHAR(255) NOT NULL, privacy boolean, time datetime, permission BOOLEAN");
-        insert=conn.prepareStatement("INSERT INTO "+DB_NAME+" (name,passwd,state) VALUES (?, ?, ?)");
-        update=conn.prepareStatement("UPDATE "+DB_NAME+" SET state=? WHERE name=?");
-        select=conn.prepareStatement("SELECT * FROM "+DB_NAME+" WHERE name=?");
-        selectConected = conn.prepareStatement("SELECT * FROM "+DB_NAME+" WHERE state=?");
+                + "size int, owner VARCHAR(255) NOT NULL, privacy boolean, time datetime DEFAULT(getdate()), permission BOOLEAN");
+        insert=conn.prepareStatement("INSERT INTO "+DB_NAME+" (name,size,owner,privacy,premission)"
+                + " VALUES (?, ?, ?, ?, ?)");
+        updateSize=conn.prepareStatement("UPDATE "+DB_NAME+" SET size=? AND time=DEFAULT WHERE name=?");
+        updatePrivacy=conn.prepareStatement("UPDATE "+DB_NAME+" SET privacy=? AND time=DEFAULT WHERE name=?");
+        updatePermission=conn.prepareStatement("UPDATE "+DB_NAME+" SET permission=? AND time=DEFAULT WHERE name=?");
+        selectByName=conn.prepareStatement("SELECT * FROM "+DB_NAME+" WHERE name=?");
+        selectByOwner=conn.prepareStatement("SELECT * FROM "+DB_NAME+" WHERE owner=?");
+        selectByNameAndOwner=conn.prepareStatement("SELECT * FROM "+DB_NAME+" WHERE name=? AND owner=?");
+        selectByPrivacy=conn.prepareStatement("SELECT * FROM "+DB_NAME+" WHERE privacy=?");
         delete=conn.prepareStatement("DELETE FROM "+DB_NAME+" WHERE name=?");
     }
     
-    public void insertRegister(String name, String passwd, String state) throws SQLException {
+    public void insertFile(String name, int size, String owner, boolean privacy, boolean permission) throws SQLException {
         insert.setString(1, name);
-        insert.setString(2, passwd);
-        insert.setString(3, state);
+        insert.setInt(2, size);
+        insert.setString(3, owner);
+        insert.setBoolean(4,privacy);
+        insert.setBoolean(5,permission);
         
         int noOfAffectedRows = insert.executeUpdate();
         System.out.println();
         System.out.println("Register inserted, changes made = " + noOfAffectedRows + " row(s).");
     }
     
-    public void updateRegister(String name, String state) throws Exception {
-        update.setString(1, state);
-        update.setString(2, name);
-        int noOfAffectedRows = update.executeUpdate();
+    public void updateSize(String name, int size) throws Exception {
+        updateSize.setInt(1, size);
+        updateSize.setString(2, name);
+        
+        int noOfAffectedRows = updateSize.executeUpdate();
         System.out.println();
         System.out.println("Register update, changes made = " + noOfAffectedRows + " row(s).");
     }
     
-    public ResultSet selectRegister(String name) throws Exception {
-        select.setString(1, name);
-        return select.executeQuery();
+    public void updatePrivacy(String name, boolean p) throws Exception {
+        updatePrivacy.setBoolean(1, p);
+        updatePrivacy.setString(2, name);
+        
+        int noOfAffectedRows = updatePrivacy.executeUpdate();
+        System.out.println();
+        System.out.println("Register update, changes made = " + noOfAffectedRows + " row(s).");
     }
     
-    public ResultSet selectByState(String state) throws SQLException {
-        select.setString(1, state);
-        return select.executeQuery();
+    public void updatePermission(String name, boolean p) throws Exception {
+        updatePermission.setBoolean(1, p);
+        updatePermission.setString(2, name);
+        
+        int noOfAffectedRows = updatePermission.executeUpdate();
+        System.out.println();
+        System.out.println("Register update, changes made = " + noOfAffectedRows + " row(s).");
     }
     
-    public void deleteRegister(String name) throws Exception {
+    public ResultSet selectByName(String name) throws Exception {
+        selectByName.setString(1, name);
+        return selectByName.executeQuery();
+    }
+    
+    public ResultSet selectByOwner(String owner) throws SQLException {
+        selectByOwner.setString(1, owner);
+        return selectByOwner.executeQuery();
+    }
+    
+    public ResultSet selectByNameAndOwner(String name, String owner) throws SQLException {
+        selectByNameAndOwner.setString(1, name);
+        selectByNameAndOwner.setString(2, owner);
+        return selectByNameAndOwner.executeQuery();
+    }
+    
+    public void deleteFile(String name) throws Exception {
         delete.setString(1, name);
         int noOfAffectedRows = delete.executeUpdate();
         System.out.println();
         System.out.println("Register update, changes made = " + noOfAffectedRows + " row(s).");
     }
 
-    public ResultSet selectAll(String name) throws Exception {
-        return statement.executeQuery("SELECT * FROM "+name);
+    public ResultSet selectAll() throws Exception {
+        return statement.executeQuery("SELECT * FROM "+DB_NAME);
     }
 
     public void dropTable() throws Exception {
