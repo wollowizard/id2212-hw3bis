3236cc4e191eb6d629063c4b820c3071ab95ece5
wollowizard <alfredo.scaccialepre@gmail.com>
2012-11-22 20:51:48 +0100

    k:

--- JavaApplication11/src/bank/Server.java
+++
@@ -1,9 +0,0 @@
-package bank;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.Set;
-
-public interface Server extends Remote {
-    
-}
--- id2212-hw2/src/id2212/hw2/bank/RejectedException.java
+++
@@ -1,9 +0,0 @@
-package id2212.hw2.bank;
-
-final public class RejectedException extends Exception {
-	private static final long serialVersionUID = -314439670131687936L;
-
-    public RejectedException(String reason) {
-        super(reason);
-    }
-}
